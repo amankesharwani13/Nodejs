eac252744178d1adca151380be3a58ffcacbb151
Aman Kesharwani <117504719+amankesharwani13@users.noreply.github.com>
2024-02-02 20:10:36 +0530

    Add files via upload

---
+++ operation.js
@@ -0,0 +1,14 @@
+const add=(a,b)=>{
+   return a+b;
+};
+
+const sub=(a,b)=>{
+   return a-b;
+};
+
+const multi=(a,b)=>{
+   return a*b;
+};
+
+const name="Aman Kesharwani";
+module.exports ={add,sub,multi,name};
