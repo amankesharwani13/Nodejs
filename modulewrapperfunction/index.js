8e3c7069af524185da8766802cd2e401c0c90287
Aman Kesharwani <117504719+amankesharwani13@users.noreply.github.com>
2024-02-09 08:56:29 +0530

    Add files via upload

---
+++ modulewrapperfunction/index.js
@@ -0,0 +1,4 @@
+console.log(__dirname); // it gives directory name
+console.log(__filename); // it gives the file name
+
+// https://www.codingninjas.com/studio/library/node-js-module-wrapper-function
